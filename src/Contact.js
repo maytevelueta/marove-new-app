cd01ade96251e00e0c2de885364861fc6b82069e
Mayte Velueta <maytevelueta@gmail.com>
2023-02-09 22:31:10 -0600

    0902

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,11 @@
 const Contact = () => {
     return ( 
         <div>
-            
+            <h2>
+                If any doubts, contact us here!
+            </h2>
         </div>
      );
 }
  
 export default Contact;
-<div>
-    
-</div>
