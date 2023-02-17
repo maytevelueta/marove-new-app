db56b670b90400ccd68e5c50f99f856e4af783ee
Mayte Velueta <maytevelueta@gmail.com>
2023-02-17 12:02:14 -0600

    1702

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Contact from './Contact';
+import Footer from '../Footer';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/contact' element={< Contact />}></Route>
       </Routes>
     </div>
+    <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <p>Follow us:</p>
+        </div>
+     );
+}
+ 
+export default Footer;
